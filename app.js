cd9ba77dab919962e12530ee92d99299a38721ae
Beruken <bielminok@gmail.com>
2024-05-30 11:36:32 -0300

    shop web with SQL

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.set('view engine', 'ejs');
-// app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
